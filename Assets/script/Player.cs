6495ce1c1f32cc2bba4ff852bab725ee9d885ee8
김규동 <2024218505@skuniv.ac.kr>
2026-05-14 14:31:58 +0900

    1
    1

---
+++ Assets/script/Player.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    [Header("Stats")]
+    [Range(0, 100)] public int Power = 1;
+    [Range(0, 500)] public int Energy = 0;
+    [Range(0, 100)] public int Unkown = 50;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    public void Attack()
+    {
+
+    }
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
